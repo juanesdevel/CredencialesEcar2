fc3ddba97f6576a7abd5e6aa0002a38dd14886e7
Juan Esteban Gallego Cano <Juanesnet2016@gmail.com>
2025-05-19 20:18:22 -0500

    Se agregan validaciones de datos a las vistas de credenciales y se agrega funcion de generar contraseña

--- ECARTemplate/obj/Debug/net6.0/ECARTemplate.AssemblyInfo.cs
+++ ECARTemplate/obj/Debug/net6.0/ECARTemplate.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECARTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc14c38ba74fa6652a92111791624ffbc0c219f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9920afe5917d9a09c5ce0e4f2725b5e12b1b28bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECARTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECARTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
